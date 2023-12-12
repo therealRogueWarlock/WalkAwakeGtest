736cf8f339c567c0ffb3667684e0b3f5bc0dba99
therealroguewarlock <vboxuser@UbuntuVM.myguest.virtualbox.org>
2023-12-12 12:20:55 +0100

    fixed upload project

---
+++ Tests/AlarmManagerTest.cpp
@@ -0,0 +1,12 @@
+//
+// Created by vboxuser on 12/12/23.
+//
+#include "gtest/gtest.h"
+
+class AlarmManagerTest : public ::testing::Test {
+
+};
+
+TEST_F(AlarmManagerTest, TestName) {
+    ASSERT_EQ(1,1);
+}
